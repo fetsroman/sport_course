bd5fd011b53118b62c5c8b06b27cdd28eb00f744
fetsroman <fets.roman@gmail.com>
2019-08-23 13:36:25 +0300

    Change Devise timeout session to 1 month

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # , :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
 
   has_many :bought_lists
   has_many :watched_lists
