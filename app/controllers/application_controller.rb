44d24b9d5f93c27b039ca458d05c11a1504d2a86
fetsroman <fets.roman@gmail.com>
2019-08-30 17:01:43 +0300

    Some small change in stylesheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class ApplicationController < ActionController::Base
   def set_locale
     # I18n.locale = extract_locale || I18n.default_locale
 
-    valid_locales=['uk','ru']
+    valid_locales = ['uk','ru']
 
     if !params[:locale].nil? && valid_locales.include?(params[:locale])
-      I18n.locale = params[:locale]
+      I18n.locale=params[:locale]
       if user_signed_in?
-        current_user.update_attribute(:locale, I18n.locale)
+        current_user.update_attribute(:locale,I18n.locale)
       end
     elsif user_signed_in? && valid_locales.include?(current_user.locale)
       I18n.locale = current_user.locale
@@ -20,12 +20,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def extract_locale
-    parsed_locale = params[:locale]
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
-  end
+  # def extract_locale
+  #   parsed_locale = params[:locale]
+  #   I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  # end
+  #
+  # def default_url_options
+  #   { locale: I18n.locale }
+  # end
 end
