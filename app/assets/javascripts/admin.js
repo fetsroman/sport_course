cae8f3cdc73d55f1251345522c3d0e508e46074a
fetsroman <fets.roman@gmail.com>
2019-09-19 16:58:22 +0300

    Change admin nad some fix

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -3,3 +3,17 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+
+$(document).ready(function() {
+    $("#image").change(function () {
+        $("#image-filename").text($(this).val().split('\\').pop());
+    });
+
+    $("#video").change(function () {
+        $("#video-filename").text($(this).val().split('\\').pop());
+    });
+
+    $("#document").change(function () {
+        $("#document-filename").text($(this).val().split('\\').pop());
+    });
+})
